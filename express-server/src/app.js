101024abf321c9e79eddb121cf0c787da283bdd9
solo-nko <solowingrazgriz@outlook.com>
2024-08-05 21:44:41 +0900

    Add and setup expressJS server

---
+++ express-server/src/app.js
@@ -0,0 +1,21 @@
+console.log("Hello, world");
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const morgan = require('morgan');
+
+// build express server
+const app = express();
+app.use(morgan('combined'));
+app.use(bodyParser.json());
+app.use(cors());
+
+
+app.get('/status', (req, res) => {
+	res.send({
+		message: 'Welcome to the server!'
+	});
+})
+
+app.listen(process.env.PORT || 8081);
