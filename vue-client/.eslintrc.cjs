7c82e7836e6a15fc5c3f6b99ea9ef1c8fe6a5654
solo-nko <solowingrazgriz@outlook.com>
2024-08-05 19:39:41 +0900

    Install vue and vue router

---
+++ vue-client/.eslintrc.cjs
@@ -0,0 +1,25 @@
+/* eslint-env node */
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  root: true,
+  'extends': [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+    '@vue/eslint-config-typescript',
+    '@vue/eslint-config-prettier/skip-formatting'
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest'
+  },
+  'rules': {
+    'indent': ['warn', 'tab'],
+    'semi': ['warn', 'always'],
+    'no-unused-vars': 'warn',
+    '@typescript-eslint/no-unused-vars': 'off',
+    'vue/no-unused-components': 'warn',
+    'vue/html-indent': ['warn', 'tab'],
+    'vue/v-on-style': ['warn', 'longform'],
+    'vue/v-bind-style': ['warn', 'longform']
+  }
+};
